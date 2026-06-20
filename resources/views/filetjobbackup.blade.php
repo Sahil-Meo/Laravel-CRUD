c603067ace357fb7168bd4fa2fba9eb9efd4ce0e
Sahil Meo <contact.sahilmeo@gmail.com>
2026-06-21 04:10:54 +0500

    Add salary breakdown sections for job titles and experience levels

--- resources/views/filetjobbackup.blade.php
+++ resources/views/filetjobbackup.blade.php
@@ -4,7 +4,12 @@
 
 <x-salary.hero :stats="$stats" :filters="$filters" :industries="$industries" :locations="$locations" :experiences="$experiences" />
 
-
+<section class="py-16 bg-white">
+    <div class="max-w-7xl mx-auto px-6 space-y-20">
+        <x-salary.salary-by-title :topTitles="$topTitles" />
+        <x-salary.by-experience :byExperience="$byExperience" />
+    </div>
+</section>
 
 <x-salary.cta />
 
